de5927bdf752b5f31628844ba859eb7eea288fe6
Matthijs Noordhof <matthijsnoordhof@hotmail.com>
2020-03-11 14:04:00 +0100

    f

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ReversiRestApi.Pages
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
